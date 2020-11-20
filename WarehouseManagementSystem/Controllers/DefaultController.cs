c70bb09d0dc6db7e5eb893b07b3dcbbb9bb098cf
vadim sieviertsev <v.northtsev@gmail.com>
2020-11-20 15:38:51 +0200

    initial commit

---
+++ WarehouseManagementSystem/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("ping")]
+        public ActionResult<string> Ping()
+        {
+            return "pong!";
+        }
+    }
+}
